1b43430f12d0bcf33ddbd3c734ee101a0169950e
mohamedsayed18 <mohamed95.a.s@gmail.com>
2020-03-11 11:03:44 +0300

    Main_idea_working

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <fstream>
 
 using namespace std;
 
@@ -8,27 +10,35 @@ int maxi = 100;
 int row = 0;
 int col = 0;
 
+map<int, pair<int, int>> direction;
+//vector<vector<int>> way;
 
-vector<vector<int>> path{
-  {0, 1, 0, 0},
-  {0, 1, 1, 0},
-  {0, 1, 1, 0},
-  {0, 1, 1, 0}
-};
+vector<vector<int>> path;
+vector<vector<int>> weights;
+vector<vector<int>> visited;
 
-vector<vector<int>> weights{
-{1, 0, 1, 1},
-{1, 0, 0, 1},
-{1, 0, 0, 1},
-{1, 0, 0, 1}
-};
-
-vector<vector<int>> visited{
-{1, 0, 1, 1},
-{1, 0, 0, 1},
-{1, 0, 0, 1},
-{1, 0, 0, 1}
-};
+void set_all()
+{
+    visited = path;
+    weights = path;
+    for (int i = 0; i < path.size(); i++)
+    {
+        for (int j = 0; j < path[0].size(); i++)
+        {
+            if(path[i][j] == 1){
+                weights[i][j] == 0;
+                visited[i][j] == 0;
+            }
+            else
+            {
+                visited[i][j] == 1;
+            }
+            
+        }
+        
+    }
+    
+}
 
 int algo()
 {
@@ -36,9 +46,9 @@ int algo()
    //create weights vector, set all possible to zero
        //two step algorithm
    //find the highest and not visited
-   for(int i=0; i<4; i++)
+   for(int i=0; i<5; i++)
    {
-      for(int j=0; j<4; j++)
+      for(int j=0; j<7; j++)
       {
          if(path[i][j])
          {
@@ -59,14 +69,16 @@ void neighbours(int i, int j)
 {
    visited[i][j] = 1;
    for(int x=-1; x<=1; x+=2)
-   {
-      if (i+x>=0 && i+x<4 && path[i+x][j] && !visited[i+x][j])
+   {  //todo check if the current weight is higher
+      if (i+x>=0 && i+x<5 && path[i+x][j] && !visited[i+x][j])
       {
          weights[i+x][j] = weights[i][j] + 1;
+         direction[weights[i+x][j]] = pair<int,int>(i,j);
       }
-      if (j+x>=0 && j+x<4 && path[i][j+x] && !visited[i][j+x])
+      if (j+x>=0 && j+x<7 && path[i][j+x] && !visited[i][j+x])
       {
          weights[i][j+x] = weights[i][j] + 1;
+         direction[weights[i][j+x]] = pair<int,int>(i,j);
       }
    }
 }
@@ -74,9 +86,9 @@ void neighbours(int i, int j)
    
 bool check(vector<vector<int>> v)
 {
-    for (int i=0; i<4; i++)
+    for (int i=0; i<5; i++)
     {
-        for (int j = 0; j < 4; j++) {
+        for (int j = 0; j < 7; j++) {
             if (v[i][j] == 0) {
                 return false;
             }
@@ -85,14 +97,72 @@ bool check(vector<vector<int>> v)
     return true;
 }
    
-
-void back_track()
-{
-    
+void printvector(vector<vector<int>> v){
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v[0].size(); j++) {
+            cout << v[i][j];
+        }
+        cout << '\n';
+    }
 }
+
+void back_track(vector<vector<int>> v)
+{
+    /* 
+    //todo use this in algo function
+    int result = 0;
+    vector<int> maxe;
+    for (int i = 0; i < 4; i++) {
+        result = *max_element(v[i].begin(), v[i].end());
+        distance(v[i].begin(), v[i].end());
+    }
+    //cout<< *max_element(maxe.begin(), maxe.end());
+   // distance(weights.begin(),  6);
+*/
+    maxi = weights[row][col];
+    row = ((*direction.find(maxi)).second).first;
+    col = ((*direction.find(maxi)).second).first;
+
+    while (maxi != 0)
+    {
+        row = ((*direction.find(maxi)).second).first;
+        col = ((*direction.find(maxi)).second).second;
+        path[row][col] = maxi;
+        maxi --;
+    }
+}
+
+vector<vector<int>> read_file(){
+    ifstream file("data.txt");
+    string line;
+    
+    vector<int> one;
+    vector<vector<int>> way;
+    while(getline(file, line)){
+        for (int i = 0; i < line.size(); i++)
+        {
+            if (line[i] == '#')
+            {
+                one.push_back(0);
+            }
+            else
+            {
+                one.push_back(1);
+            }
+        }
+        way.push_back(one);
+        one.clear(); 
+    }
+    return way;
+}
+
+
 int main()
 {
+    path = read_file();
+    //set_all();
     bool done = false;
+    /*
     while(!done)
     {
         //find heighst not visited
@@ -101,12 +171,9 @@ int main()
         neighbours(row, col);
         done = check(visited);
     }
-    
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            cout << weights[i][j];
-        }
-        cout << '\n';
-    }
+    //back_track(weights);
+    back_track(weights);
+    printvector(path);
     return 0;
+*/
 }
